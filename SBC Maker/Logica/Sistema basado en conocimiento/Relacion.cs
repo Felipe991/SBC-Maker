e40b9365d9fe051970dcc80e3ca17db1210272f9
Felipe Barrientos Rojas <felipeignacio.barrientos@userena.cl>
2022-11-30 19:17:03 -0300

    Cambios logica relacion

--- SBC Maker/Logica/Sistema basado en conocimiento/Relacion.cs
+++ SBC Maker/Logica/Sistema basado en conocimiento/Relacion.cs
@@ -3,12 +3,14 @@
     public class Relacion
     {
         private Nodo nodo;
-        
-        public Relacion(Nodo nodo)
+        private List<string>? respuestasNecesarias;
+        public Relacion(Nodo nodo, List<string> respuestasNecesarias = null)
         {
             this.Nodo = nodo;
+            this.RespuestasNecesarias = respuestasNecesarias;
         }
 
         public Nodo Nodo { get => nodo; set => nodo = value; }
+        public List<string> RespuestasNecesarias { get => respuestasNecesarias; set => respuestasNecesarias = value; }
     }
 }
